2db8883a2e4d6824de81e365584d24791f7dbc92
seafrexindia-prog <seafrexindia@gmail.com>
2025-12-04 18:09:24 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,6 @@ if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
-console.log('Seafrex Portal: Application Starting...');
-
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
