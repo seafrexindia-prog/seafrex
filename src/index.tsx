167a95e82b2627edb454789e2f4604af129be19e
seafrexindia-prog <seafrexindia@gmail.com>
2025-12-04 17:35:41 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.tsx'; // Assuming you might have css file later, but standard Vite apps have this
+
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
